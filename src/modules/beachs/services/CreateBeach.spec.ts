8d070d46cf4d5ebc117985bd3df53182c176970e
Uriel Mori <moriuriel@gmail.com>
2022-07-06 22:27:40 -0300

    test: create testing module

--- src/modules/beachs/services/CreateBeach.spec.ts
+++ src/modules/beachs/services/CreateBeach.spec.ts
@@ -1,9 +1,9 @@
+import { Test } from '@nestjs/testing';
 import { CreateBeach, GeoPosition } from '../interfaces/Beachs.interfaces';
-import { BeachRepository } from '../repositories';
+import { BeachRepository, MongoDbBeachRepository } from '../repositories';
 import { CreateBeachService } from './CreateBeach.service';
 
 let createBeach: CreateBeachService;
-let beachRepository: BeachRepository;
 
 const beachEntity: CreateBeach = {
   name: 'Praia do Rosa - SC',
@@ -12,10 +12,20 @@ const beachEntity: CreateBeach = {
   lng: -48.8167,
 };
 describe('Create Beach Service', () => {
-  beforeEach(() => {
-    beachRepository = new BeachRepository();
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        CreateBeachService,
+        {
+          provide: MongoDbBeachRepository,
+          useFactory: () => {
+            return new BeachRepository();
+          },
+        },
+      ],
+    }).compile();
 
-    createBeach = new CreateBeachService(beachRepository);
+    createBeach = module.get(CreateBeachService);
   });
   test('should be create a beach', async () => {
     const createBeachSpy = jest.spyOn(createBeach, 'execute');
